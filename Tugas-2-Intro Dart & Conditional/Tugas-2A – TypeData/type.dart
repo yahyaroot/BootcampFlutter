8365636d7f1b8228133bed541a7749a0f513f27a
“yahyaroot” <amriyahya.email@gmail.com>
2023-11-07 22:02:09 +0800

    Pengumpulan Tugas 2

---
+++ Tugas-2-Intro Dart & Conditional/Tugas-2A – TypeData/type.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+void main() {
+  // Soal No. 1 (Membuat kalimat)
+  // var word = 'dart';
+  // var second = 'is';
+  // var third = 'awesome';
+  // var fourth = 'and';
+  // var fifth = 'I';
+  // var sixth = 'love';
+  // var seventh = 'it!';
+
+  // print(word);
+  // print(second);
+  // print(third);
+  // print(fourth);
+  // print(fifth);
+  // print(sixth);
+  // print(seventh);
+
+// Soal No.2 Mengurai kalimat (Akses karakter dalam string),
+  // var sentence = "I am going to be Flutter Developer";
+  // var exampleFirstWord = sentence[0];
+  // var exampleSecondWord = sentence[2] + sentence[3];
+  // var thirdWord =
+  //     sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9];
+  // var fourthWord = sentence[11] + sentence[12];
+  // var fifthWord = sentence[14] + sentence[15];
+  // var sixthWord = sentence[16] +
+  //     sentence[17] +
+  //     sentence[18] +
+  //     sentence[19] +
+  //     sentence[20] +
+  //     sentence[21] +
+  //     sentence[22] +
+  //     sentence[23] +
+  //     sentence[24];
+  // var seventhWord = sentence[25] +
+  //     sentence[26] +
+  //     sentence[27] +
+  //     sentence[28] +
+  //     sentence[29] +
+  //     sentence[30] +
+  //     sentence[31] +
+  //     sentence[32] +
+  //     sentence[33];
+
+  // print('First Word: ' + exampleFirstWord);
+  // print('Second Word: ' + exampleSecondWord);
+  // print('Third Word: ' + thirdWord);
+  // print('Fourth Word: ' + fourthWord);
+  // print('Fifth Word: ' + fifthWord);
+  // print('Sixth Word: ' + sixthWord);
+  // print('Seventh Word: ' + seventhWord);
+
+// Soal 3 Dengan menggunakan I/O pada dart buatlah input dinamis yang akan menginput nama depan dan belakang dan jika di enterakan menggabungkan nama depan dan belakang
+  // print("Masukan Nama Depan:\t ");
+
+  // String? first_name = stdin.readLineSync();
+
+  // print("Masukan Nama Belakang:\t ");
+
+  // String? second_name = stdin.readLineSync();
+
+  // print("Nama Lenkap Kamu Adalah:, $first_name $second_name ");
+
+// Soal 4 Dengan menggunakan operator operasikan variable berikut ini menjadi bentuk operasi perkalian, penjumlahan, pengurangan dan pembagian a = 5,  b = 10 jadi misal a * b = 5 * 10 = 50 dst.
+  // int a = 5;
+  // int b = 10;
+
+  // int perkalian = a * b;
+  // double pembagian = a / b;
+  // int penambahan = a + b;
+  // int pengurangan = a - b;
+
+  // print("Perkalian: $perkalian");
+  // print("Perkalian: $pembagian");
+  // print("Perkalian: $penambahan");
+  // print("Perkalian: $pengurangan");
+}
