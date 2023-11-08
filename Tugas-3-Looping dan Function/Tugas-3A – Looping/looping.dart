cbd5c33dd08c028bdd5840f99c47dadb59cebfcb
“yahyaroot” <amriyahya.email@gmail.com>
2023-11-08 20:18:43 +0800

    Pengumpulan Tugas 3

---
+++ Tugas-3-Looping dan Function/Tugas-3A – Looping/looping.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+void main() {
+  // Soal. 1 Looping While
+  // print("LOOPING PERTAMA");
+  // int i = 2;
+  // while (i <= 20) {
+  //   print("$i - I love coding");
+  //   i += 2;
+  // }
+
+  // print("LOOPING KEDUA");
+  // int j = 20;
+  // while (j >= 1) {
+  //   print("$j - I will become a mobile developer");
+  //   j -= 2;
+  // }
+
+// Soal. 2 Looping menggunakan for
+  // for (int i = 1; i <= 20; i++) {
+  //   if (i % 2 == 0) {
+  //     print("$i - Berkualitas");
+  //   } else if (i % 3 == 0) {
+  //     print("$i - I Love Coding");
+  //   } else {
+  //     print("$i - Santai");
+  //   }
+  // }
+
+// No. 3 Membuat Persegi Panjang #
+  // int height = 4;
+  // int width = 8;
+
+  // for (int i = 0; i < height; i++) {
+  //   for (int j = 0; j < width; j++) {
+  //     stdout.write("#");
+  //   }
+  //   print(""); // Move to the next line after each row of '#' symbols
+  // }
+
+  //No. 4 Membuat Tangga
+
+  // int height = 7;
+  // for (int i = 1; i <= height; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     stdout.write("#");
+  //   }
+  //   print(""); // Pindah ke baris berikutnya setelah satu baris selesai
+  // }
+}
---
+++ Tugas-3-Looping dan Function/Tugas-3B – Function/function.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+void main() {
+  // Soal 1
+  // teriak() {
+  //   return "Halo Sanbers!";
+  // }
+
+  // print(teriak());
+
+  // Soal 2
+  // int kalikan(int num1, int num2) {
+  //   return num1 * num2;
+  // }
+
+  // var num1 = 12;
+  // var num2 = 4;
+
+  // var hasilKali = kalikan(num1, num2);
+  // print(hasilKali);
+
+  // Soal No 3
+  // String introduce(String name, int age, String address, String hobby) {
+  //   return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+  // }
+
+  // var name = "Agus";
+  // var age = 30;
+  // var address = "Jln. Malioboro, Yogyakarta";
+  // var hobby = "Gaming";
+
+  // var perkenalan = introduce(name, age, address, hobby);
+  // print(perkenalan);
+
+  // Soal 4
+  // int factorial(int n) {
+  //   if (n <= 0) {
+  //     return 1;
+  //   } else {
+  //     return n * factorial(n - 1);
+  //   }
+  // }
+
+  // stdout.write("Masukkan angka untuk menghitung faktorial: ");
+  // String? inputStr = stdin.readLineSync();
+
+  // if (inputStr != null) {
+  //   int input = int.tryParse(inputStr) ?? 0;
+
+  //   if (input >= 0) {
+  //     int hasilFaktorial = factorial(input);
+  //     print('$input! = $hasilFaktorial');
+  //   } else {
+  //     print("Angka harus non-negatif.");
+  //   }
+  // } else {
+  //   print("Masukan tidak valid.");
+  // }
+}
