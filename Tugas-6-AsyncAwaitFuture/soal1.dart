1aac05a4ef1d48b353331ff199eb26ba6919cf19
“yahyaroot” <amriyahya.email@gmail.com>
2023-11-14 00:24:07 +0800

    Tugas AsyncAwaitFuture

--- Tugas-6-AsyncAwaitFuture/soal1.dart
+++ Tugas-6-AsyncAwaitFuture/soal1.dart
@@ -1,21 +1,3 @@
-// void main() {
-//   var h = Human();
-//   print("Luffy");
-//   print("zoro");
-//   print("killer");
-//   h.getData();
-//   print(h.name);
-// }
-
-// class Human {
-//   String name = "nama character one piece";
-
-//   void getData() {
-//     name = "hilmy";
-//     print("get data [done]");
-//   }
-// }
-
 void main() async {
   var h = Human();
   print("Luffy");
